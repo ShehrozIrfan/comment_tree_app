a0be666e99adacf4a9b3556a3a3d7e68a5f125ee
Shehroz Irfan <>
2022-10-02 19:45:14 +0500

    Self referential association added - CRUD added to Comment and its responses

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  # Adding self referential associations
+  belongs_to :parent, class_name: "Comment", optional: true
+  has_many :responses, class_name: "Comment", foreign_key: "parent_id", dependent: :destroy
 end
