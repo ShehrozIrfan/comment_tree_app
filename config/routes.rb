dfb3b5010f75f9d43c33247986c31ae14b7274f8
Shehroz Irfan <36946330+ShehrozIrfan@users.noreply.github.com>
2022-10-02 13:26:44 +0500

    Revert "Model created, CRUD added for comment"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  root 'comments#index'
-
-  resources :comments
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
