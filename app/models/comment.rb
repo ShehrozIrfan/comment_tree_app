b86cdb58cffc1e581d17665471ee5717b023578b
Shehroz Irfan <shehrozirfan89@gmail.com>
2022-11-06 18:41:54 +0500

    [Add Validation on Comment Model] Validation added on comments model, text is required, minimum length is 10 and maximum is 500

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   # Adding self referential associations
   belongs_to :parent, class_name: "Comment", optional: true
   has_many :responses, class_name: "Comment", foreign_key: "parent_id", dependent: :destroy
+
+  validates :text, presence: true, length: {minimum: 10, maximum: 500}
 end
