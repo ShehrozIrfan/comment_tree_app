01d5f7b22a1eb075851f90e89d2f6f76c77172c1
Shehroz Irfan <shehrozirfan89@gmail.com>
2022-11-20 11:38:14 +0500

    [Association] Association added for user and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :parent, class_name: "Comment", optional: true
   has_many :responses, class_name: "Comment", foreign_key: "parent_id", dependent: :destroy
 
+  belongs_to :user
+
   validates :text, presence: true, length: {minimum: 10, maximum: 500}
 end
