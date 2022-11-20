1bdabe84229a70d544bf68f7869ce4d992314f64
Shehroz Irfan <shehrozirfan89@gmail.com>
2022-11-20 18:37:56 +0500

    [Adding username] Validations added for first name and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :comments, dependent: :destroy
+
+  validates :first_name, :last_name, presence: true, length: {minimum: 3, maximum: 12}
 end
