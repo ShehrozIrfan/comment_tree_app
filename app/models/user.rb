295f12a6bc5fc4974fff6bad702bb039259aacfd
Shehroz Irfan <shehrozirfan89@gmail.com>
2022-11-26 12:22:33 +0500

    [Adding Avatar] Association added to user model and required modifications are made in signup and account update views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :comments, dependent: :destroy
+  has_one_attached :avatar
 
   validates :first_name, :last_name, presence: true, length: {minimum: 3, maximum: 12}
 end
