e4f7568cf73962ae82de4cfb054534fad9480d25
Shehroz Irfan <>
2022-12-10 20:53:44 +0500

    [Adding GraphQL] Comment type added

---
+++ app/graphql/types/comment_type.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Types
+  class CommentType < Types::BaseObject
+    field :id, ID, null: false
+    field :text, String, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :parent_id, Integer, null: true
+    field :user_id, Integer, null: true
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,13 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :comments,
+    [Types::CommentType],
+    null: false,
+    description: "Returns a list of comments"
+
+    def comments
+      Comment.all
     end
   end
 end
