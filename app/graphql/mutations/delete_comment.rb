cc1c2a9c9357c122eb7acf342ccc4570bf1e8bf2
Shehroz Irfan <>
2022-12-10 23:13:34 +0500

    [Adding GraphQL Mutations] Delete: added

---
+++ app/graphql/mutations/delete_comment.rb
@@ -0,0 +1,23 @@
+module Mutations
+  class DeleteComment < Mutations::BaseMutation
+    argument :id, ID, required: true
+
+    field :comment, Types::CommentType, null: true
+    field :error, String, null: true
+
+    def resolve(id:)
+      comment = Comment.find_by_id id
+      if comment && comment.destroy
+        {
+          comment: comment,
+          error: ""
+        }
+      else
+        {
+          comment: nil,
+          error: "Something went wrong. Please try again."
+        }
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_comment, mutation: Mutations::CreateComment
+    field :delete_comment, mutation: Mutations::DeleteComment
   end
 end
